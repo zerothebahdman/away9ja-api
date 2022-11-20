1f62faa25693ba21ef677e418ab9e47ea9715f72
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2022-11-20 23:28:12 +0100

    Feature: refactored login class

--- src/authentication/LoginUser.ts
+++ src/authentication/LoginUser.ts
@@ -2,29 +2,54 @@ import { NextFunction, Request, Response } from 'express';
 import AppException from '../exceptions/AppException';
 import httpStatus from 'http-status';
 import AuthService from '../services/Auth.service';
+import { User } from '@prisma/client';
+import UserService from '../services/User.service';
+import EncryptionService from '../services/Encryption.service';
 
 export default class LoginUser {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+    private readonly encryptionService: EncryptionService
+  ) {}
   async _loginUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const { accessToken, refreshToken, user } =
-        await this.authService.loginUser(req.body, next);
+      const _userExists: User = await this.userService.getUserByEmail(
+        req.body.email
+      );
+
+      if (
+        !_userExists ||
+        !(await this.encryptionService.comparePassword(
+          _userExists.password,
+          req.body.password
+        ))
+      )
+        throw new Error(`Oops!, invalid email or password`);
+
+      if (_userExists.isEmailVerified !== true)
+        next(
+          new AppException(
+            'Oops! email address is not verified',
+            httpStatus.FORBIDDEN
+          )
+        );
+      const token = await this.authService.loginUser(_userExists);
       return res.status(httpStatus.ACCEPTED).json({
-        status: 'success',
-        user,
-        accessToken,
-        refreshToken,
+        user: _userExists,
+        token,
       });
     } catch (err: any) {
-      return next(new AppException(err.status, err.message));
+      return next(
+        new AppException(err.message, err.status || httpStatus.BAD_REQUEST)
+      );
     }
   }
 
   async regenerateAccessToken(req: Request, res: Response, next: NextFunction) {
     try {
       const accessToken = await this.authService.regenerateAccessToken(
-        req.body.refreshToken,
-        next
+        req.body.refreshToken
       );
       if (!accessToken || accessToken.trim() === '')
         return next(
@@ -33,7 +58,9 @@ export default class LoginUser {
 
       return res.status(httpStatus.OK).json({ status: 'success', accessToken });
     } catch (err: any) {
-      return next(new AppException(err.status, err.message));
+      return next(
+        new AppException(err.message, err.status || httpStatus.BAD_REQUEST)
+      );
     }
   }
 

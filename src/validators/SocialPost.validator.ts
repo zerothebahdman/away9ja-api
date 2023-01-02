89851ca0814d8cb73f9cdf032bf33ac51414f073
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2023-01-02 09:42:45 +0100

    chore: made changes to schema file and create post validator method

--- src/validators/SocialPost.validator.ts
+++ src/validators/SocialPost.validator.ts
@@ -1,12 +1,13 @@
 import Joi from 'joi';
 import { objectId } from './Custom.validator';
+import { PostType } from '../../config/constants';
 
 export const CreatePostValidator = {
   body: Joi.object().keys({
     body: Joi.string().lowercase().max(150).messages({
       'string.max': 'You have exceeded more than 150 characters',
     }),
-    post_type: Joi.string().valid('text', 'image', 'video'),
+    post_type: Joi.string().valid(...Object.values(PostType)),
     location: Joi.string().min(3).lowercase().max(20),
     images: Joi.array().items(Joi.string()).optional(),
     tags: Joi.array().items(Joi.string()).optional(),

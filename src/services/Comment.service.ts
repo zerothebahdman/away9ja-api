7de8892bac2d64190e29f4d61e19372da9ccf06b
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2023-01-03 21:12:05 +0100

    fix: fixed issue in comment service

--- src/services/Comment.service.ts
+++ src/services/Comment.service.ts
@@ -1,20 +1,20 @@
-import { Post_Comment } from '@prisma/client';
+import { PostComment } from '@prisma/client';
 import prisma from '../database/model.module';
 
 export default class CommentService {
   async getAllComments(filter: unknown) {
-    const data = await prisma.comment.findMany({ where: filter });
+    const data = await prisma.postComment.findMany({ where: filter });
     return data;
   }
 
   async getCommentById(id: string) {
     const filter: { id: string; deleted_at: null } = { id, deleted_at: null };
-    const data = await prisma.comment.findUnique({ where: { ...filter } });
+    const data = await prisma.postComment.findUnique({ where: { ...filter } });
     return data;
   }
 
-  async createComment(comment: Post_Comment) {
-    const data = await prisma.comment.create({ data: comment });
+  async createComment(comment: PostComment) {
+    const data = await prisma.postComment.create({ data: comment });
     return data;
   }
 }

77edd514b15f65fa580adeded5c2f8100b811ceb
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2023-02-02 12:31:44 +0100

    feat: implemented sentry for error tracking

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import enforce from 'express-sslify';
@@ -7,6 +8,8 @@ import {
   ErrorConverter,
 } from './middlewares/error_handler.middleware';
 import morgan from 'morgan';
+import Sentry from '@sentry/node';
+import Tracing from '@sentry/tracing';
 import hpp from 'hpp';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
@@ -15,6 +18,37 @@ import httpStatus from 'http-status';
 import config from '../../config/default';
 
 const app: Application = express();
+
+interface MiddlewareError extends Error {
+  status?: number | string;
+  statusCode?: number | string;
+  status_code?: number | string;
+  output?: {
+    statusCode?: number | string;
+  };
+}
+
+if (config.sentry.enabled) {
+  Sentry.init({
+    dsn: config.sentry.dsn,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+
+  // RequestHandler creates a separate execution context using domains, so that every
+  // transaction/span/breadcrumb is attached to its own Hub instance
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+}
 
 if (config.env === 'production' || config.env === 'staging') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
@@ -54,16 +88,32 @@ app.all('*', (req: Request, _res: Response, next: NextFunction) => {
   );
 });
 
+if (config.sentry.enabled) {
+  // The error handler must be before any other error middleware and after all controllers
+  app.use(
+    Sentry.Handlers.errorHandler({
+      shouldHandleError(error: MiddlewareError) {
+        // Capture some 40-errors and >= 500 errors
+        const CAPTURE_STATUS_CODES = [400, 401];
+        if (
+          CAPTURE_STATUS_CODES.includes(error.statusCode as number) ||
+          (error.statusCode as number) >= 500
+        ) {
+          return true;
+        }
+        return false;
+      },
+    }),
+  );
+
+  // Optional fallthrough error handler
+  app.use(function onError(_err: Error, _req: Request, res: Response | any) {
+    // The error id is attached to `res.sentry` to be returned and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(`${res.sentry as Response}\n`);
+  });
+}
+
 app.use(ErrorConverter);
 app.use(ErrorHandler);
 export default app;
-
-// class App {
-//   public app: Application;
-//   public port: number;
-
-//   constructor() {
-//     this.app = express();
-//     this.port = config.get<number>('port');
-//   };
-// }
